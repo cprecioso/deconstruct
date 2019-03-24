bc9b2b44fc28ebcc25ca642ab2e76042af996c8d
Carlos Precioso <kalprestito@gmail.com>
2019-03-25 00:06:18 +0100

    Add offsetForElement to ListDeconstructor

--- src/deconstructors/list.ts
+++ src/deconstructors/list.ts
@@ -3,11 +3,13 @@ import { Deconstructor } from "../types"
 export function list<T>(
   times: number,
   deconstructor: Deconstructor<T>
-): Deconstructor<ReadonlyArray<T>> {
-  return new ListDeconstructor(times, deconstructor)
+): ListDeconstructor<T> {
+  return new _ListDeconstructor(times, deconstructor)
 }
 
-class ListDeconstructor<T> implements Deconstructor<ReadonlyArray<T>> {
+export type ListDeconstructor<T> = _ListDeconstructor<T>
+
+class _ListDeconstructor<T> implements Deconstructor<ReadonlyArray<T>> {
   bytes = this._inner.bytes ? this._inner.bytes * this._times : undefined
 
   constructor(
@@ -15,6 +17,9 @@ class ListDeconstructor<T> implements Deconstructor<ReadonlyArray<T>> {
     public readonly _inner: Deconstructor<T>
   ) {}
 
+  /**
+   * @internal
+   */
   _fromBuffer(buffer: Buffer, offset: number) {
     const values: T[] = []
     let accumulatedOffset = offset
@@ -26,5 +31,9 @@ class ListDeconstructor<T> implements Deconstructor<ReadonlyArray<T>> {
     }
 
     return { value: values, bytesUsed: accumulatedOffset - offset }
+  }
+
+  offsetForElement(nth: number) {
+    return this._inner.bytes && this._inner.bytes * nth
   }
 }
