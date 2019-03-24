9742132a0e0bd266766993fb935556b25fad45c2
Carlos Precioso <kalprestito@gmail.com>
2019-03-24 23:45:03 +0100

    Add support for variable length RawDeconstructor

--- src/deconstructors/raw.ts
+++ src/deconstructors/raw.ts
@@ -1,16 +1,17 @@
 import { Deconstructor } from "../types"
 
-export function raw(bytes: number): Deconstructor<Buffer> {
+export function raw(bytes?: number): Deconstructor<Buffer> {
   return new RawDeconstructor(bytes)
 }
 
 class RawDeconstructor implements Deconstructor<Buffer> {
-  constructor(public readonly bytes: number) {}
+  constructor(public readonly bytes?: number) {}
 
   _fromBuffer(buffer: Buffer, offset: number) {
-    return {
-      value: buffer.slice(offset, offset + this.bytes),
-      bytesUsed: this.bytes
-    }
+    const value = buffer.slice(
+      offset,
+      this.bytes ? offset + this.bytes : undefined
+    )
+    return { value, bytesUsed: value.byteLength }
   }
 }
