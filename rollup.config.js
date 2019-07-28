c2f7f6cfccb5dc1312c0b52e5e861d74638689fa
Carlos Precioso <kalprestito@gmail.com>
2019-07-29 01:33:55 +0200

    Improve build process
    Add rollup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import dts from "rollup-plugin-dts"
+import typescript from "rollup-plugin-typescript2"
+
+/** @type {import("rollup").RollupOptions[]} */
+const config = [
+  {
+    input: "src/index.ts",
+    output: [
+      { file: "lib/index.js", format: "cjs" },
+      { file: "lib/index.mjs", format: "es" }
+    ],
+    plugins: [
+      typescript({
+        typescript: require("typescript"),
+        cacheRoot: "node_modules/.cache/rpts2"
+      })
+    ]
+  },
+  {
+    input: "src/index.ts",
+    output: { file: "lib/index.d.ts", format: "es" },
+    plugins: [dts()]
+  }
+]
+
+export default config
