abeab85d157b5f2eafd454edc0f74ec61effa987
Carlos Precioso <kalprestito@gmail.com>
2019-07-29 04:07:13 +0200

    Add byte check in `fromBuffer`

--- src/fromBuffer.ts
+++ src/fromBuffer.ts
@@ -5,5 +5,12 @@ export function fromBuffer<T>(
   buf: Buffer,
   offset = 0
 ): T {
+  if (buf.length < (deconstructor.bytes || deconstructor.minBytes))
+    throw new Error(
+      `Buffer (${
+        buf.length
+      } bytes) is too short for the created Deconstructor (${deconstructor.bytes ||
+        deconstructor.minBytes} bytes)`
+    )
   return deconstructor._fromBuffer(buf, offset).value
 }
