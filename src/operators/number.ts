fa4d9583881f55b7dfbf46fd73a731f78c091ed0
Carlos Precioso <kalprestito@gmail.com>
2019-07-30 16:20:20 +0200

    Add support for 64-byte number and bigint

--- src/operators/number.ts
+++ src/operators/number.ts
@@ -1,16 +1,17 @@
 import { Deconstruction, Deconstructor } from "../types"
 
-class NumberDeconstructor implements Deconstructor<number> {
+class NumberDeconstructor<T extends bigint | number>
+  implements Deconstructor<T> {
   constructor(
     public readonly bytes: number,
-    protected _fn: (this: Buffer, offset: number, byteLength: number) => number
+    protected _fn: (this: Buffer, offset: number) => T
   ) {}
 
   readonly minBytes = this.bytes
 
-  _fromBuffer(buffer: Buffer, offset: number): Deconstruction<number> {
+  _fromBuffer(buffer: Buffer, offset: number): Deconstruction<T> {
     return {
-      value: this._fn.call(buffer, offset, this.bytes),
+      value: this._fn.call(buffer, offset),
       bytesUsed: this.bytes
     }
   }
@@ -18,60 +19,98 @@ class NumberDeconstructor implements Deconstructor<number> {
 
 const b = Buffer.prototype
 
-const _u8 = new NumberDeconstructor(1, b.readUIntLE)
+const _u8: Deconstructor<number> = new NumberDeconstructor(1, b.readUInt8)
 /** Extracts an unsigned 8-byte integer */
 export const u8 = () => _u8
 
-const _i8 = new NumberDeconstructor(1, b.readIntLE)
+const _i8: Deconstructor<number> = new NumberDeconstructor(1, b.readInt8)
 /** Extracts an signed 8-byte integer */
 export const i8 = () => _i8
 
-const _i16LE = new NumberDeconstructor(2, b.readIntLE)
+const _i16LE: Deconstructor<number> = new NumberDeconstructor(2, b.readInt16LE)
 /** Extracts an little-endian signed 16-byte integer */
 export const i16LE = () => _i16LE
 
-const _i16BE = new NumberDeconstructor(2, b.readIntBE)
+const _i16BE: Deconstructor<number> = new NumberDeconstructor(2, b.readInt16BE)
 /** Extracts an big-endian signed 16-byte integer */
 export const i16BE = () => _i16BE
 
-const _u16LE = new NumberDeconstructor(2, b.readUIntLE)
+const _u16LE: Deconstructor<number> = new NumberDeconstructor(2, b.readUInt16LE)
 /** Extracts an little-endian unsigned 16-byte integer */
 export const u16LE = () => _u16LE
 
-const _u16BE = new NumberDeconstructor(2, b.readUIntBE)
+const _u16BE: Deconstructor<number> = new NumberDeconstructor(2, b.readUInt16BE)
 /** Extracts an big-endian unsigned 16-byte integer */
 export const u16BE = () => _u16BE
 
-const _i32LE = new NumberDeconstructor(4, b.readIntLE)
+const _i32LE: Deconstructor<number> = new NumberDeconstructor(4, b.readInt32LE)
 /** Extracts an little-endian signed 32-byte integer */
 export const i32LE = () => _i32LE
 
-const _i32BE = new NumberDeconstructor(4, b.readIntBE)
+const _i32BE: Deconstructor<number> = new NumberDeconstructor(4, b.readInt32BE)
 /** Extracts an big-endian signed 32-byte integer */
 export const i32BE = () => _i32BE
 
-const _u32LE = new NumberDeconstructor(4, b.readUIntLE)
+const _u32LE: Deconstructor<number> = new NumberDeconstructor(4, b.readUInt32LE)
 /** Extracts an little-endian unsigned 32-byte integer */
 export const u32LE = () => _u32LE
 
-const _u32BE = new NumberDeconstructor(4, b.readUIntBE)
+const _u32BE: Deconstructor<number> = new NumberDeconstructor(4, b.readUInt32BE)
 /** Extracts an big-endian unsigned 32-byte integer */
 export const u32BE = () => _u32BE
 
-const _floatLE = new NumberDeconstructor(4, b.readFloatLE)
+const _i64LE: Deconstructor<bigint> = new NumberDeconstructor(
+  8,
+  b.readBigInt64LE
+)
+/** Extracts an little-endian signed 32-byte integer */
+export const i64LE = () => _i64LE
+
+const _i64BE: Deconstructor<bigint> = new NumberDeconstructor(
+  8,
+  b.readBigInt64BE
+)
+/** Extracts an big-endian signed 32-byte integer */
+export const i64BE = () => _i64BE
+
+const _u64LE: Deconstructor<bigint> = new NumberDeconstructor(
+  8,
+  b.readBigUInt64LE
+)
+/** Extracts an little-endian unsigned 32-byte integer */
+export const u64LE = () => _u64LE
+
+const _u64BE: Deconstructor<bigint> = new NumberDeconstructor(
+  8,
+  b.readBigUInt64BE
+)
+/** Extracts an big-endian unsigned 32-byte integer */
+export const u64BE = () => _u64BE
+
+const _floatLE: Deconstructor<number> = new NumberDeconstructor(
+  4,
+  b.readFloatLE
+)
 /** Extracts an little-endian float */
 export const floatLE = () => _floatLE
 
-const _floatBE = new NumberDeconstructor(4, b.readFloatBE)
+const _floatBE: Deconstructor<number> = new NumberDeconstructor(
+  4,
+  b.readFloatBE
+)
 /** Extracts an big-endian float */
 export const floatBE = () => _floatBE
 
-const _doubleLE = new NumberDeconstructor(8, b.readDoubleLE)
+const _doubleLE: Deconstructor<number> = new NumberDeconstructor(
+  8,
+  b.readDoubleLE
+)
 /** Extracts an little-endian double */
 export const doubleLE = () => _doubleLE
 
-const _doubleBE = new NumberDeconstructor(8, b.readDoubleBE)
+const _doubleBE: Deconstructor<number> = new NumberDeconstructor(
+  8,
+  b.readDoubleBE
+)
 /** Extracts an big-endian double */
 export const doubleBE = () => _doubleBE
-
-// TODO Add BigInt support (64 byte)
