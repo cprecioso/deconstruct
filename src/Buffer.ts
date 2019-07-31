01b61e107a7da05b8503e09201e9c3df93590825
Carlos Precioso <kalprestito@gmail.com>
2019-07-31 16:11:17 +0200

    Do not depend on `typedarray-to-buffer`

--- src/Buffer.ts
+++ src/Buffer.ts
@@ -1,8 +1,40 @@
-import typedarrayToBuffer from "typedarray-to-buffer"
+export type InputBuffer = ArrayBufferLike | ArrayBufferView | number[] | string
 
-export type InputBuffer = typedarrayToBuffer.AcceptedInputValue
 export type OutputBuffer = Buffer
 
+function isArrayBufferLike(v: any): v is ArrayBuffer | SharedArrayBuffer {
+  return v instanceof ArrayBuffer || v instanceof SharedArrayBuffer
+}
+
+function isArrayBufferView(v: any): v is ArrayBufferView {
+  return (
+    v &&
+    v.buffer &&
+    v.byteLength &&
+    v.byteOffset &&
+    isArrayBufferLike(v.buffer) &&
+    typeof v.byteLength === "number" &&
+    typeof v.byteOffset === "number"
+  )
+}
+
 export function normalizeBuffer(input: InputBuffer): OutputBuffer {
-  return typedarrayToBuffer(input)
+  if (isArrayBufferView(input)) {
+    const buf = Buffer.from(input.buffer)
+    if (input.byteLength !== input.buffer.byteLength) {
+      const start = input.byteOffset
+      const end = input.byteOffset + input.byteLength
+      return buf.slice(start, end)
+    } else {
+      return buf
+    }
+
+    // FIXME The differente branches are needed so TypeScript won't complain
+  } else if (typeof input === "string") {
+    return Buffer.from(input)
+  } else if (Array.isArray(input)) {
+    return Buffer.from(input)
+  } else {
+    return Buffer.from(input)
+  }
 }
