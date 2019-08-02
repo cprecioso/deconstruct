c26f00b5c51e32f528d4de9595adf13d4a06846d
Carlos Precioso <kalprestito@gmail.com>
2019-08-02 13:29:34 +0200

    Do not slice in fromBuffer

--- src/fromBuffer.ts
+++ src/fromBuffer.ts
@@ -1,23 +1,22 @@
-import { Deconstructor } from "./types"
+import { Deconstruction, Deconstructor } from "./types"
 import { InputBuffer, normalizeBuffer } from "./util"
 
 /** Executes a Deconstructor on a Buffer, and returns its result */
 export function fromBuffer<T>(
   deconstructor: Deconstructor<T>,
   input: InputBuffer,
-  offset = 0,
-  length?: number
-): T {
+  offset = 0
+): Deconstruction<T> {
   const buf = normalizeBuffer(input)
-  if (buf.length < (deconstructor.bytes || deconstructor.minBytes))
+
+  if (buf.length < (deconstructor.bytes || deconstructor.minBytes)) {
     throw new Error(
       `Buffer (${
         buf.length
       } bytes) is too short for the created Deconstructor (${deconstructor.bytes ||
         deconstructor.minBytes} bytes)`
     )
-  return deconstructor._fromBuffer(
-    buf.slice(offset, length != null ? offset + length : undefined),
-    0
-  ).value
+  }
+
+  return deconstructor._fromBuffer(buf, offset)
 }
