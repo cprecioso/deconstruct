4009de929628021ce9ea6ebadfd528bf131e5eaa
Carlos Precioso <kalprestito@gmail.com>
2020-02-07 20:51:38 +0100

    Migrate to @wessberg/rollup-plugin-ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,20 @@
-import dts from "rollup-plugin-dts"
-import typescript from "rollup-plugin-typescript2"
+// @ts-check
 
-/** @type {import("rollup").RollupOptions[]} */
-const config = [
-  {
-    input: "src/index.ts",
-    output: [
-      { file: "lib/index.js", format: "cjs" },
-      { file: "lib/index.es.js", format: "es" }
-    ],
-    plugins: [
-      typescript({
-        typescript: require("typescript"),
-        cacheRoot: "node_modules/.cache/rpts2"
-      })
-    ],
-    external: [
-      ...require("module").builtinModules,
-    ]
-  },
-  {
-    input: "src/index.ts",
-    output: { file: "lib/index.d.ts", format: "es" },
-    plugins: [dts()]
-  }
-]
+import ts from "@wessberg/rollup-plugin-ts"
+
+/** @type {import("rollup").RollupOptions} */
+const config = {
+  input: "src/index.ts",
+  output: [
+    { file: "lib/index.js", format: "cjs" },
+    { file: "lib/index.mjs", format: "es" }
+  ],
+  plugins: [ts({ transpiler: "babel" })],
+  external: [
+    ...require("module").builtinModules,
+    // @ts-ignore
+    ...Object.keys(require("./package.json").dependencies || {})
+  ]
+}
 
 export default config
