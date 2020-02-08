77e969a3bf590cc76f91d066a1af4a25a3e82fc9
Carlos Precioso <kalprestito@gmail.com>
2020-02-08 13:02:08 +0100

    StructPairs `pairs` property is protected

--- src/deconstructors/complex/struct-pairs.ts
+++ src/deconstructors/complex/struct-pairs.ts
@@ -27,20 +27,20 @@ export function structPairs<T extends DeconstructorPair>(
 
 class StructPairsDeconstructor<T extends DeconstructorPair>
   implements ComplexDeconstructor<Values<T>, Keys<T>> {
-  constructor(protected pairs: readonly T[]) {}
+  constructor(protected _pairs: readonly T[]) {}
 
-  readonly bytes = this.pairs.reduce((sum, cur) => {
+  readonly bytes = this._pairs.reduce((sum, cur) => {
     const bytes = cur[1].bytes
     return sum == null || bytes == null ? undefined : sum + bytes
   }, 0 as number | undefined)
 
-  readonly minBytes = this.pairs.reduce((sum, cur) => sum + cur[1].minBytes, 0)
+  readonly minBytes = this._pairs.reduce((sum, cur) => sum + cur[1].minBytes, 0)
 
   _fromBuffer(buffer: OutputBuffer, offset: number): Deconstruction<Values<T>> {
     let currentOffset = offset
     const result: Partial<Values<T>> = {}
 
-    for (const [key, deconstructor] of this.pairs) {
+    for (const [key, deconstructor] of this._pairs) {
       const { bytesUsed, value } = deconstructor._fromBuffer(
         buffer,
         currentOffset
@@ -54,7 +54,7 @@ class StructPairsDeconstructor<T extends DeconstructorPair>
 
   offsetForElement(key: Keys<T>): number | undefined {
     let offset = 0
-    for (const pair of this.pairs) {
+    for (const pair of this._pairs) {
       if (pair[0] === key) return offset
       if (pair[1].bytes == null) return undefined
       offset += pair[1].bytes
