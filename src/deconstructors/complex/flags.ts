212bd1aedb3c97a6baee2348caff70ef8b8e933c
Carlos Precioso <kalprestito@gmail.com>
2020-02-08 12:21:15 +0100

    Add `structPairs` deconstructor

--- src/deconstructors/complex/flags.ts
+++ src/deconstructors/complex/flags.ts
@@ -1,12 +1,9 @@
 import { ComplexDeconstructor } from "../../types"
 import { boolean } from "../simple"
-import { struct } from "./struct"
+import { structPairs } from "./struct-pairs"
 
 export function flags<T extends string>(
   flagNames: readonly T[]
 ): ComplexDeconstructor<Record<T, boolean>, T> {
-  return flagNames.reduce(
-    (struct, flagName) => struct.field(flagName, boolean()),
-    struct()
-  )
+  return structPairs(flagNames.map(flagName => [flagName, boolean] as const))
 }
---
+++ src/deconstructors/complex/struct-pairs.ts
@@ -0,0 +1,63 @@
+import {
+  ComplexDeconstructor,
+  Deconstruction,
+  Deconstructor
+} from "../../types"
+import { OutputBuffer } from "../../util"
+
+type DeconstructorPair<K extends string = string, T = any> = readonly [
+  K,
+  Deconstructor<T>
+]
+
+type Keys<T extends DeconstructorPair> = T extends DeconstructorPair<infer U>
+  ? U
+  : never
+
+type Values<T extends DeconstructorPair, K extends string = Keys<T>> = {
+  [P in K]: T extends DeconstructorPair<P, infer V> ? V : never
+}
+
+export function structPairs<T extends DeconstructorPair>(
+  pairs: readonly T[]
+): ComplexDeconstructor<Values<T>, Keys<T>> {
+  return new StructPairsDeconstructor(pairs)
+}
+
+class StructPairsDeconstructor<T extends DeconstructorPair>
+  implements ComplexDeconstructor<Values<T>, Keys<T>> {
+  constructor(protected pairs: readonly T[]) {}
+
+  readonly bytes = this.pairs.reduce((sum, cur) => {
+    const bytes = cur[1].bytes
+    return sum == null || bytes == null ? undefined : sum + bytes
+  }, 0 as number | undefined)
+
+  readonly minBytes = this.pairs.reduce((sum, cur) => sum + cur[1].minBytes, 0)
+
+  _fromBuffer(buffer: OutputBuffer, offset: number): Deconstruction<Values<T>> {
+    let currentOffset = offset
+    const result: Partial<Values<T>> = {}
+
+    for (const [key, deconstructor] of this.pairs) {
+      const { bytesUsed, value } = deconstructor._fromBuffer(
+        buffer,
+        currentOffset
+      )
+      currentOffset += bytesUsed
+      result[key as Keys<T>] = value
+    }
+
+    return { bytesUsed: currentOffset, value: result as Values<T> }
+  }
+
+  offsetForElement(key: Keys<T>): number | undefined {
+    let offset = 0
+    for (const pair of this.pairs) {
+      if (pair[0] === key) return offset
+      if (pair[1].bytes == null) return undefined
+      offset += pair[1].bytes
+    }
+    return undefined
+  }
+}
